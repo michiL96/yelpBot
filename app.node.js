b1fda1111565ef7cb9e7771a564601c322dde870
Michela Lorandi <michela.lorandi@studenti.unitn.it>
2018-04-24 15:45:33 +0200

    log added

--- app.node.js
+++ app.node.js
@@ -16,6 +16,7 @@ app.get('/yelpBot', function(req, res) {
 });
 
 app.post('/yelpBot', function(req, res) {
+  console.log("here");
   if (req.body.queryResult.action === 'search') {
     var term = req.body.queryResult.parameters.terms;
     var location = req.body.queryResult.parameters.location;
