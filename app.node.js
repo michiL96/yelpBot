259023c9c8a2213f63157263065922004c574f9f
Michela Lorandi <michela.lorandi@studenti.unitn.it>
2018-04-24 14:08:42 +0200

    function search added

--- app.node.js
+++ app.node.js
@@ -7,27 +7,40 @@ app.use(bodyParser.json());
 const yelp = require('yelp-fusion');
 const apiKey = process.env.API_KEY;
 
-const searchRequest = {
-  term:'Four Barrel Coffee',
-  location: 'san francisco, ca'
-};
-
 const client = yelp.client(apiKey);
 
 var requestsArray = [];
 
 app.get('/', function(req, res) {
-  requestsArray.push({varProva:'ciao ciao'});
-
-  client.search(searchRequest).then(response => {
-    const firstResult = response.jsonBody.businesses[0];
-    const prettyJson = JSON.stringify(firstResult, null, 4);
-    console.log(prettyJson);
-  }).catch(e => {
-    console.log(e);
-  });
-  
   res.send(requestsArray);
+});
+
+app.post('/', function(req, res) {
+  if (req.body.queryResult.action === 'search') {
+    var term = req.body.queryResult.parameters.terms;
+    var location = req.body.queryResult.parameters.location;
+
+    var searchRequest = {
+      term: term,
+      location: location
+    };
+    client.search(searchRequest).then(response => {
+      const firstResult = response.jsonBody.businesses[0];
+      const prettyJson = JSON.stringify(firstResult, null, 4);
+      console.log(response);
+      //console.log(prettyJson);
+      res.setHeader('Content-Type', 'application/json');
+      res.send(JSON.stringify({
+        speech: '',
+        messages: [{
+          type: 0,
+          speech: 'Ricerca completata'
+        }]
+      }));
+    }).catch(e => {
+      console.log(e);
+    });
+  }
 });
 
 module.exports = app;
