aa1f17ea7325f3ba57aa84f7681f52547bc263dd
Michela Lorandi <michela.lorandi@studenti.unitn.it>
2018-04-24 13:50:05 +0200

    add yelp fusion

--- app.node.js
+++ app.node.js
@@ -4,10 +4,29 @@ var app = express();
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
+const yelp = require('yelp-fusion');
+const apiKey = process.env.API_KEY;
+
+const searchRequest = {
+  term:'Four Barrel Coffee',
+  location: 'san francisco, ca'
+};
+
+const client = yelp.client(apiKey);
+
 var requestsArray = [];
 
 app.get('/', function(req, res) {
-  requestsArray.push({varProva:'ciao ciao'})
+  requestsArray.push({varProva:'ciao ciao'});
+
+  client.search(searchRequest).then(response => {
+    const firstResult = response.jsonBody.businesses[0];
+    const prettyJson = JSON.stringify(firstResult, null, 4);
+    console.log(prettyJson);
+  }).catch(e => {
+    console.log(e);
+  });
+  
   res.send(requestsArray);
 });
 
